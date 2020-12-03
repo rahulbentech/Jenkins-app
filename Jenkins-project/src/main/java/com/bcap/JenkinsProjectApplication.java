5508fcf2f418b199b13e85a46184dbe1e1487882
BEN <BEN@DESKTOP-AB79HQ0>
2020-12-03 21:27:08 +0530

    Jenkins first commit

---
+++ Jenkins-project/src/main/java/com/bcap/JenkinsProjectApplication.java
@@ -0,0 +1,25 @@
+package com.bcap;
+
+import javax.annotation.PostConstruct;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsProjectApplication {
+	
+	private static Logger logger = LoggerFactory.getLogger(JenkinsProjectApplication.class);
+	
+	@PostConstruct
+	public void init() {
+		logger.info("Application started...");
+	}
+
+	public static void main(String[] args) {
+		logger.info("Application executed...");
+		SpringApplication.run(JenkinsProjectApplication.class, args);
+	}
+
+}
---
+++ Jenkins-project/src/test/java/com/bcap/JenkinsProjectApplicationTests.java
@@ -0,0 +1,22 @@
+package com.bcap;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinsProjectApplicationTests {
+	
+	private static Logger logger = LoggerFactory.getLogger(JenkinsProjectApplication.class);
+
+
+	@Test
+	void contextLoads() {
+		logger.info("Executing test cases...");
+		assertEquals(true, true);
+	}
+
+}
